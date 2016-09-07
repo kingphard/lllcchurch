80293c60639e766c7fe68a86ba70a919c4c0b870
ABDUSALAAM PHARDLULAHI <ABDUSALAAM PHARDLULAHI>
2016-09-07 19:04:57 +0100

    my new app

---
+++ resources/views/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>www.lcc.org</title>
+    <link href="{{ URL::asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ URL::asset('font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+    <link href="{{ URL::asset('css/plugins/iCheck/custom.css')}}" rel="stylesheet">
+    <link href="{{ URL::asset('css/animate.css')}}" rel="stylesheet">
+    <!--<link href="{{ URL::asset('css/style.css')}}" rel="stylesheet">-->
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="col-md-4">
+            LOGO STAYS HERE
+        </div>
+        <div class="col-md-8">
+            CHURCH NAME 
+        </div>
+
+            <div class="wrapper">
+                <div class="content">
+                    <p>Love Crusade Chapel headquater church located in kano state, Nigeria West Afrca. it is a chhurch with the commission of deliverance, properiity and preparing people for heaven, the encouter for each of these came at different times during the preparation period for the ministry.<br/>
+                    We have what we call commission declaration such as "God of my covenant apear for me" this is borne out of the threefold covenant which we renew every first snday evening of each month.
+                    we also have another declaraion which is "There is victory always in Jesus". The last one which we recite as benediction "I receive grace and deterination from Jesus, for the kind of holiness of life that will take me to heaven at the end of my journey on earh in the name of Jesus.</p>               
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
+<script src="{{ URL::asset('js/bootstrap.js')}}"></script>
+<script src="{{ URL::asset('js/inspinia.js')}}"></script>
+<script src="{{ URL::asset('js/jquery-2.2.4.min.js')}}"></script>
+<script src="{{ URL::asset('js/jquery.metisMenu.js')}}"></script>
+<script src="{{ URL::asset('js/npm.js')}}"></script>
+    
+    </body>
+</html>
